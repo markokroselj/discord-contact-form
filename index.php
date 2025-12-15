388e73f3a8b0acf78bbed84ca59cb5bc663a52af
Marko Krošelj <48656423+markokroselj@users.noreply.github.com>
2025-12-15 23:19:56 +0100

    Remove / from form link

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
     <div class="shadow-md rounded-md w-fit m-5 mx-auto p-6 ring-1 ring-slate-800">
         <h1 class="text-3xl pb-4">Contact us</h1>
-        <form id="contactForm" class="flex gap-2" hx-post="/sendMsg.php" hx-target="#response" hx-target-4xx="#response" hx-swap="innerHTML">
+        <form id="contactForm" class="flex gap-2" hx-post="sendMsg.php" hx-target="#response" hx-target-4xx="#response" hx-swap="innerHTML">
             <div class="md:w-72">
                 <div class="relative w-full min-w-[200px] h-10">
                     <input type="text" required class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900" id="msg" autocomplete="off" placeholder="" name="msg">
